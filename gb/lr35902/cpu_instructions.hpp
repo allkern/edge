fe7658b65f298bb738f23dd3be8dc2d426268283
Lycoder <aurealinbox@gmail.com>
2022-10-10 11:58:30 -0300

    Tidy up instructions, implement ld a, (bc)

--- gb/lr35902/cpu_instructions.hpp
+++ gb/lr35902/cpu_instructions.hpp
@@ -12,6 +12,25 @@
 // Use (void) to silence unused warnings.
 #define assertm(exp, msg) assert(((void)msg, exp))
 
+#define A cpu->r[7]
+#define B cpu->r[0]
+#define C cpu->r[1]
+#define D cpu->r[2]
+#define E cpu->r[3]
+#define F cpu->r[6]
+#define H cpu->r[4]
+#define L cpu->r[5]
+#define AF (((uint16_t)A << 8) | F)
+#define BC (((uint16_t)B << 8) | C)
+#define DE (((uint16_t)D << 8) | E)
+#define HL (((uint16_t)H << 8) | L)
+
+#define INVALID_M \
+    default: { \
+        _log(error, "Invalid M cycle %u while executing %s", cpu->ex_m_cycle, __FUNCTION__); \
+        std::exit(1); \
+    } break;
+
 namespace gb {
     instruction_state_t nop(cpu_t* cpu) {
         return IS_LAST_CYCLE;
@@ -49,11 +68,7 @@ namespace gb {
                 return IS_LAST_CYCLE;
             } break;
 
-            default: {
-                _log(error, "Invalid M cycle %u while executing %s", cpu->ex_m_cycle, __FUNCTION__);
-
-                std::exit(1);
-            } break;
+            INVALID_M;
         }
 
         return IS_DONE;
@@ -63,7 +78,7 @@ namespace gb {
         switch (cpu->ex_m_cycle) {
             case 0: {
                 if (!cpu->read_ongoing) {
-                    cpu_init_read(cpu, ((uint16_t)cpu->r[4] << 8) | cpu->r[5]);
+                    cpu_init_read(cpu, HL);
                 }
 
                 if (!cpu_handle_read(cpu, &cpu->l_latch)) {
@@ -81,11 +96,7 @@ namespace gb {
                 return IS_LAST_CYCLE;
             } break;
 
-            default: {
-                _log(error, "Invalid M cycle %u while executing %s", cpu->ex_m_cycle, __FUNCTION__);
-
-                std::exit(1);
-            } break;
+            INVALID_M;
         }
 
         return IS_DONE;
@@ -97,7 +108,7 @@ namespace gb {
                 if (!cpu->write_ongoing) {
                     cpu->x_latch = (cpu->i_latch >> 3) & 0x7;
 
-                    cpu_init_write(cpu, ((uint16_t)cpu->r[4] << 8) | cpu->r[5], cpu->r[cpu->x_latch]);
+                    cpu_init_write(cpu, HL, cpu->r[cpu->x_latch]);
                 }
 
                 if (!cpu_handle_write(cpu)) {
@@ -111,11 +122,7 @@ namespace gb {
                 return IS_LAST_CYCLE;
             } break;
 
-            default: {
-                _log(error, "Invalid M cycle %u while executing %s", cpu->ex_m_cycle, __FUNCTION__);
-
-                std::exit(1);
-            } break;
+            INVALID_M;
         }
 
         return IS_DONE;
@@ -137,7 +144,7 @@ namespace gb {
 
             case 1: {
                 if (!cpu->write_ongoing) {
-                    cpu_init_write(cpu, ((uint16_t)cpu->r[4] << 8) | cpu->r[5], cpu->l_latch);
+                    cpu_init_write(cpu, HL, cpu->l_latch);
                 }
 
                 if (!cpu_handle_write(cpu)) {
@@ -151,13 +158,50 @@ namespace gb {
                 return IS_LAST_CYCLE;
             } break;
 
-            default: {
-                _log(error, "Invalid M cycle %u while executing %s", cpu->ex_m_cycle, __FUNCTION__);
+            INVALID_M;
+        }
 
-                std::exit(1);
+        return IS_DONE;
+    }
+
+    instruction_state_t ld_a_bc(cpu_t* cpu) {
+        switch (cpu->ex_m_cycle) {
+            case 0: {
+                if (!cpu->read_ongoing) {
+                    cpu_init_read(cpu, BC);
+                }
+
+                if (!cpu_handle_read(cpu, &cpu->l_latch)) {
+                    cpu->ex_m_cycle++;
+                }
+
+                return IS_EXECUTING;
             } break;
+
+            case 1: {
+                A = cpu->l_latch;
+
+                return IS_LAST_CYCLE;
+            } break;
+
+            INVALID_M;
         }
 
         return IS_DONE;
     }
 }
+
+#undef A
+#undef B
+#undef C
+#undef D
+#undef E
+#undef F
+#undef H
+#undef L
+#undef AF
+#undef BC
+#undef DE
+#undef HL
+
+#undef INVALID_M
